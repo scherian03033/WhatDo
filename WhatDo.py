46a8511532e77cf1c90db0a6f9cceec272a5e5c9
Sanjay Cherian <scherian@charter.net>
2013-07-22 11:00:54 -0400

    First pushed version
    Trivial python program to suggest activities when you get bored.

--- WhatDo.py
+++ WhatDo.py
@@ -1,14 +1,55 @@
+#!/usr/bin/python
+import sys
 from random import randint
+import datetime
+
+def monthDay2num(m,d):
+    return m * 100 + d
+
 
 ## Evolve this to tag each item with a season bitmap (i.e numbers 1-15)
+##  1 = Winter 12/21-3/19
+##  2 = Spring 3/20-6/20
+##  4 = Summer 6/21-9/21
+##  8 = Fall 9/22-12/20
+##  15 = year round
 
-filename = "whatdo.txt"
+SeasonLookup = [[3,19,1],[6,20,2],[9,21,3],[12,20,4],[12,31,1]]
+SeasonLookupLen = len(SeasonLookup)
+SeasonNames = ["Winter", "Spring", "Summer", "Fall"]
+
+if (len(sys.argv) == 1):
+	filename = "whatdo.txt"
+else:
+	filename = sys.argv[1]
 
 f = open(filename)
 lines = f.readlines()
 
 listLen = len(lines)
 
-idx = randint(0,listLen-1)
+today = datetime.date.today()
+# For Testing:
+# today = datetime.date(2013,12,21)
+# print "Today is",today
 
-print "\nWhy don't you", lines[idx]
+season = 15
+
+for i in range(SeasonLookupLen):
+    if (monthDay2num(today.month,today.day) <= monthDay2num(SeasonLookup[i][0],SeasonLookup[i][1])):
+        season = SeasonLookup[i][2]
+        break
+
+print "Season is", SeasonNames[season-1]
+
+done = False
+
+while (done == False):
+    idx = randint(0,listLen-1)
+    parseLine = lines[idx].split(";")
+    if (int(parseLine[0]) & season):
+        idea = parseLine[1]
+        done = True
+
+print "\nYou might like to", idea[0:len(idea)-1]+"?\n"
+
